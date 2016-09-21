2dbaa2311f71603089140c0733095193c299fb21
Wout B <40796@psdschools.org>
2016-09-20 22:44:57 -0600

    Working on 3 in java

---
+++ java/3.java
@@ -0,0 +1,24 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void p(String text)
+	{
+		System.out.println(text);
+	}
+
+	public static int largestPrimeFactor(int num)
+	{
+		int i = 2;
+		while(i * i <= num)
+		{
+
+		}
+		return 0;
+	}
+
+}
