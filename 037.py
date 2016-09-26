3e4e8ecb113267fc4f3d9a84d0632bb5dc142ff3
Wout B <40796@psdschools.org>
2016-09-25 22:20:58 -0600

    It's giving an error and I'm done

--- 037.py
+++ 037.py
@@ -11,13 +11,17 @@ def primeFind(number):
 
 def checkLeft(number):
 	tmp = True
-	for i in range(2, len(number)):
-		number.pop(i)
-		if not primeFind(int(number)):
+	lit = []
+	for i in range(1, len(str(number))):
+		lit.append(int( (str(number)[-1]) )
+
+	for i in range(2, int(len(lit)) ):
+		del (lit[i - 1])
+		if not primeFind(int(lit)):
 			tmp = False
 	return tmp
 
 # for i in range(8, 500000):
 # 	if primeFind(i):
 
-print(checkLeft(str(3797)))
+print(str(checkLeft(3797)))
